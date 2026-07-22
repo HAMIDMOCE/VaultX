647806a30099dad4d88a711c7c59f98a3e5a2eda
Hamid <hamid.mohammadian.ce@gmail.com>
2026-07-22 16:05:26 +0330

    Add database cleanup method

--- src/database.py
+++ src/database.py
@@ -8,27 +8,40 @@ class DatabaseManager:
             port=3306,
             user="root",
             password="Hamid.mo83",
-            database_name="vaultx"
-            ):
-        self.database_name = database_name
-        self.port = port
+            database_name="vaultx"):
+        
         self.host = host
+        self.port = port
         self.user = user
         self.password = password
+        self.database_name = database_name
+
         self.connection = None
         self.cursor = None
 
-    def connect(self):
+    def connect_server(self):
         try:
             self.connection = connector.connect(
                 host = self.host,
+                port = self.port,
                 user = self.user,
                 password = self.password,
-                port = self.port,
-                database = self.database_name
             )
-            self.cursor = self.connection.cursor()
-            return True, "Connected successfully"
 
+            self.cursor = self.connection.cursor()
+            return True, "Connected to MySQL server successfully."
+        
         except connector.Error as error:
             return False, str(error)
+
+    def create_database(self):
+        pass
+
+    def connect_database(self):
+        pass
+
+    def create_tables(self):
+        pass
+
+    def close(self):
+        pass
